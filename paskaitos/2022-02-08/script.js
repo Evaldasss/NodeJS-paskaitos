b7aabac3247011dd362501dc50cf98fca6c99b2e
MarcinkEve <marcinke@gmail.com>
2022-02-08 22:36:58 +0200

    3-task started

--- paskaitos/2022-02-08/script.js
+++ paskaitos/2022-02-08/script.js
@@ -19,17 +19,19 @@ o kitas iš porinių;
 0 jeigu jie didesni už 15;
 */
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max + 1) - 0) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 let array = new Array(30).fill(0);
-let newArray = array.map(el => getRandomNumber(5, 10))
+let newArray = array.map((el) => getRandomNumber(5, 25));
 console.log(newArray);
+
+
 
 console.log("------------1------------");
 /*1. Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;*/
 
-let moreThan10 = newArray.filter(el => el > 10).length;
+let moreThan10 = newArray.filter((el) => el > 10).length;
 console.log(moreThan10);
 
 
@@ -57,10 +59,10 @@ console.log("------------3------------");
 
 let evenIndexesValueSum = 0;
 
-for(let i = 0; i < newArray.length; i++){
-    if(i % 2 === 0) {
-        evenIndexesValueSum += newArray[i];
-    }
+for (let i = 0; i < newArray.length; i++) {
+  if (i % 2 === 0) {
+    evenIndexesValueSum += newArray[i];
+  }
 }
 console.log(evenIndexesValueSum);
 
@@ -79,8 +81,8 @@ console.log("------------5------------");
 /*5. Papildykite masyvą papildomais 10 elementų su reikšmėmis nuo 5 iki 25,
 kad bendras masyvas padidėtų iki indekso 39; */
 
-for(let i = 0; i < 10; i++){
-    newArray.push(getRandomNumber(5, 25));
+for (let i = 0; i < 10; i++) {
+  newArray.push(getRandomNumber(5, 25));
 }
 console.log(newArray);
 
@@ -94,7 +96,9 @@ o kitas iš porinių;*/
 let evenValues = [];
 let oddValues = [];
 
-newArray.filter((el, i) => i % 2 === 0 ? evenValues.push(el) : oddValues.push(el));
+newArray.filter((el, i) =>
+  i % 2 === 0 ? evenValues.push(el) : oddValues.push(el)
+);
 
 console.log(evenValues);
 console.log(oddValues);
@@ -105,19 +109,19 @@ console.log("------------7------------");
 /*7. Pirminio masyvo elementus su poriniais indeksais padarykite lygius 
 0 jeigu jie didesni už 15;*/
 console.log("--------------------7.1var.");
-    for (let i = 0; i < newArray.length; i++) {
-      if (i % 2 === 0 && newArray[i] > 15) {
-        newArray[i] = 0;
-      } else {
-      }
-    }
-    console.log(newArray);
-    
+for (let i = 0; i < newArray.length; i++) {
+  if (i % 2 === 0 && newArray[i] > 15) {
+    newArray[i] = 0;
+  } else {
+  }
+}
+console.log(newArray);
+
 
 
 console.log("--------------------7.2var.");
 
-newArray.map((el, i) => (i % 2 === 0 && el > 15) ? el = 0 : el);
+newArray.map((el, i) => (i % 2 === 0 && el > 15 ? (el = 0) : el));
 console.log(newArray);
 
 
@@ -125,12 +129,100 @@ console.log(newArray);
 
 console.log("---------------UZDUOTIS-2");
 
-/*
-1. Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai 
+console.log("------------1------------");
+/*1. Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai 
 nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios 
-savo masyve (t.y. neturi kartotis).
-2. Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra 
-pirmajame masyve, bet nėra antrame masyve.
-3. Sugeneruokite masyvą iš elementų, kurie kartojasi abiejuose masyvuose.
-4. Sugeneruokite masyvą, kurio indeksus sudarytų pirmojo masyvo reikšmės,
-o jo reikšmės būtų antrojo masyvo.*/
+savo masyve (t.y. neturi kartotis).*/
+
+let array1 = [];
+let array2 = [];
+
+function numberGenerator(arr, min, max, length) {
+  if (arr.length >= length) {
+    return;
+  }
+  let randNumber = Math.floor(Math.floor(Math.random() * (max - min) + min));
+  if (arr.indexOf(randNumber) < 0) {
+    arr.push(randNumber);
+  }
+  numberGenerator(arr, min, max, length);
+}
+numberGenerator(array1, 100, 999, 100);
+numberGenerator(array2, 100, 999, 100);
+
+console.log(array1);
+console.log(array2);
+
+
+
+console.log("------------2------------");
+/*2. Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, 
+kurios yra pirmajame masyve, bet nėra antrame masyve.*/
+let array3 = [];
+
+for (let i = 0; i < array1.length; i++) {
+  if (!array2.includes(array1[i])) {
+    array3.push(array1[i]);
+  }
+}
+console.log(array3);
+
+
+
+console.log("------------3------------");
+/*3. Sugeneruokite masyvą iš elementų, kurie kartojasi 
+abiejuose masyvuose.*/
+
+let arr1arr2 = array1.concat(array2);
+console.log(arr1arr2);
+
+/*Cia lyginami visu masyvo elementu indeksai su pirmu rastu
+to elemento indeksu: jei indeksai nesutampa, vadinasi elementas 
+kartojasi (yra dublikatas)*/
+let array4 = arr1arr2.filter((el, i, arr) => arr.indexOf(el) !== i);
+console.log(array4);
+
+
+
+console.log("------------4------------");
+/*4. Sugeneruokite masyvą, kurio indekso numeriai butu pirmojo 
+masyvo reikšmės, o naujo masyvo reikšmės būtų antrojo masyvo 
+elementai.*/
+
+let array5 = [];
+
+for (let i = 0; i < array1.length; i++) {
+  array5[array1[i]] = array2[i];
+}
+console.log(array5);
+
+console.log("max", Math.max(...array1));
+
+
+
+
+console.log("---------------UZDUOTIS-3");
+
+/*Sugeneruokite 101 elemento masyvą su atsitiktiniais skaičiais nuo 
+0 iki 300. */
+
+let construct = new Array(101).fill(0);
+let masyvas = construct.map((el) => getRandomNumber(0, 300));
+console.log(masyvas);
+
+
+
+console.log("------------1------------");
+/*/*1. Reikšmes kurios tame masyve yra ne unikalios pergeneruokite 
+iš naujo taip, kad visos reikšmės masyve būtų unikalios. */
+
+console.log("------------2------------");
+/*2. Išrūšiuokite masyvą taip, kad jo didžiausia reikšmė būtų masyvo 
+viduryje, o einant nuo jos link masyvo pradžios ir pabaigos 
+reikšmės mažėtų. */
+
+console.log("------------3------------");
+/*3. Paskaičiuokite pirmos ir antros masyvo dalies sumas (neskaičiuojant 
+vidurinės). Jeigu sumų skirtumas (modulis, absoliutus dydis) yra 
+didesnis nei | 30 | rūšiavimą kartokite. (Kad sumos nesiskirtų viena 
+nuo kitos daugiau nei per 30)*/
