4bbb0ba8bea0be16d71b4e327b78978fd55c784a
MarcinkEve <marcinke@gmail.com>
2022-02-19 22:54:00 +0200

    updated

--- paskaitos/2022-02-18/backend/index.js
+++ paskaitos/2022-02-18/backend/index.js
@@ -1,10 +1,14 @@
 //const express = require('express')
-import express from 'express'
-import cors from 'cors'
 
-const app = express()
+import express from "express";
+import cors from "cors";
+import Krepsinis from "./krepsinis.js";
 
-app.use(cors())
+
+const app = express();
+
+app.use(cors());
+app.use("/img", express.static("img"));
 
 app.use( 
   express.urlencoded({
@@ -14,9 +18,113 @@ app.use(
 
 app.use(express.json())   //automatinis stringo konvertavimas i objekta
 
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/*************************
+function date() {
+  const date = new Date();
+  const [month, day, year] = [
+    date.getMonth(),
+    date.getDate(),
+    date.getFullYear(),
+  ];
+  let data = date.toDateString();
+  //console.log("DATA", data);
+  return data;
+}
+
+
+//sita funkcija rodo einamaji laika, jei ji priskirtume prie  let objektas1 = {time: showTime()}
+function showTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  //const seconds = date.getSeconds();
+
+  const adjustedHours = hours < 10 ? "0" + hours : hours;
+  const adjustedMinutes = minutes < 10 ? "0" + minutes : minutes;
+  //const adjustedSeconds = seconds < 10 ? "0" + seconds : seconds;
+  //const time = adjustedHours + ":" + adjustedMinutes + ":" + adjustedSeconds;
+  const time = adjustedHours + ":" + adjustedMinutes;
+
+  return time;
+}
+
+
+
+app.get("/", function (req, res) {
+  let objektas1 = {
+    round: getRandomNumber(1, 32),
+    date: date(),
+    stadium: "Peace And Friendship Stadium",
+    time: showTime(),
+    team: {
+      first: "Olympiacos Piraeus",
+      second: "Anadolu Efes Istanbul",
+    },
+    logo: {
+      flogo: "http://localhost:3001/img/Olympiacos.png",
+      slogo: "http://localhost:3001/img/Istanbul.png"
+    },
+    result: {
+      first: 0,
+      second: 0,
+    },
+    quarter: {
+      number: "1st quarter",
+      final: "final",
+    },
+  };
+
+  res.json(objektas1);
+});
+
+
+
+//dinaminis variantas, rezultatas random, ne didejantis
+
+app.get("/result", function (req, res) {
+  let objektas2 = {
+    result: {
+      first: getRandomNumber(1, 100),      
+      second: getRandomNumber(1, 100),
+    },
+    quarter: getRandomNumber(1, 5),
+    time: showTime(),
+  };
+
+  res.json(objektas2);
+});
+*/////////////////////////////////
+
+// rezultatas pagal klase 'Krepsinis'
+/*konstanta skirta klases inicijavimui ir pavertimui objektu 
+(aprasoma virs route'o, nes jei aprasysim route, tai vis nuo nulio 
+taskus rodys ir nedides taskai)*/
+const rezultatas = new Krepsinis();
+//console.log(rezultatas);
+
+app.get("/checkscore", function (req, res) {
+  let obj = {
+    logo: {
+      flogo: "http://localhost:3001/img/Olympiacos.png",
+      slogo: "http://localhost:3001/img/Istanbul.png"
+    }
+  };
+
+  res.json({rezultatas, obj});
+});
+
+
+
+
 app.post('/post-request', (req, res) => {
  // let data = JSON.parse(req.body)  //uzkomentuota, nes apsirasem 'app.use(express.json())'
-  console.log(req.body)
+  console.log(req.body);
+  
   res.json(req.body)
 })
 
--- paskaitos/2022-02-18/frontend/app.js
+++ paskaitos/2022-02-18/frontend/app.js
@@ -1,17 +1,143 @@
+//console.log("app.js pajungtas");
+
+/****
+const renewData = () => {
+  fetch("http://localhost:3001") // puslapis, is kurio imama informacija
+    .then((response) => response.json()) //json formato responsa konvertuoja i js objekta
+    //console.log(response.json());       //zr. rezultata narsykles konsolej
+    .then((jsonObjektas) => {
+      //console.log(jsonObjektas.round.match);
+     
+      document.querySelector(".round").innerText = `Round ${jsonObjektas.round}`;
+      document.querySelector(".match-stage").innerHTML = jsonObjektas.quarter.final.toUpperCase();
+      document.querySelector(".date").innerHTML = jsonObjektas.date;
+
+      document.querySelector(".stadium").innerHTML = jsonObjektas.stadium;
+      document.querySelector(".time").innerHTML = jsonObjektas.time;
+      //document.querySelector(".one").innerHTML = `<img class="logo one" src="${jsonObjektas.logo.flogo}" alt="logo">`;
+      //document.querySelector(".two").innerHTML = `<img class="logo two" src="${jsonObjektas.logo.slogo}" alt="logo">`;
+
+      document.querySelector(".first").innerHTML = jsonObjektas.team.first;
+      document.querySelector(".second").innerHTML = jsonObjektas.team.second;
+      document.querySelector(".f-scores").innerHTML = jsonObjektas.result.first;
+      document.querySelector(".s-scores").innerHTML = jsonObjektas.result.second;
+    });
+};
+renewData();
+
+
+//dinaminis variantas, rezultatas random, ne didejantis
+
+const checkData = () => {
+  fetch("http://localhost:3001/result")
+    .then((response) => response.json())
+    .then((jsonObjektas) => {
+        document.querySelector(".time").innerHTML = jsonObjektas.time;
+      
+      // document.querySelector(".f-scores").style.color = "#fff";
+      // document.querySelector(".s-scores").style.color = "#fff";
+
+      // document.querySelector(".f-scores").innerHTML = jsonObjektas.result.first;
+      // document.querySelector(".s-scores").innerHTML = jsonObjektas.result.second;
+     
+      // if (jsonObjektas.result.first > jsonObjektas.result.second) {
+      //   document.querySelector(".f-scores").style.color = "#00F2A7";
+      // } else {
+      //   document.querySelector(".s-scores").style.color = "#00F2A7";
+      // }
+
+      //  if(jsonObjektas.quarter === 1){
+      //   document.querySelector(".match-stage").innerHTML = `${jsonObjektas.quarter}st quarter`;
+      //  } else if (jsonObjektas.quarter === 2){
+      //   document.querySelector(".match-stage").innerHTML = `${jsonObjektas.quarter}nd quarter`;
+      //  }else if (jsonObjektas.quarter === 3){
+      //   document.querySelector(".match-stage").innerHTML = `${jsonObjektas.quarter}rd quarter`;
+      //  }else if (jsonObjektas.quarter === 4){
+      //   document.querySelector(".match-stage").innerHTML = `${jsonObjektas.quarter}th quarter`;
+      //  }
+      //console.log(jsonObjektas.quarter);
+    
+    });
+
+  //console.log('test Zodis')
+  setTimeout(() => {
+    checkData();
+  }, 60000);
+};
+checkData();
+/*
+document.querySelector(".button").addEventListener("click", () => {
+  //console.log('clicked')  //spaudziam mygtuka narsyklej ir patikrinam konsolej, ar pajungtas eventListener
+  checkData();
+});
+*/
+
+// rezultatas pagal klase 'Krepsinis'
+const play = () => {
+  fetch("http://localhost:3001/checkscore")
+    .then((response) => response.json())
+    .then((jsonObjektas) => {
+      //console.log("checkscore", jsonObjektas);
+      //console.log(jsonObjektas.kelinys);
+
+      document.querySelector(".f-scores").style.color = "#fff";
+      document.querySelector(".s-scores").style.color = "#fff";
+
+      document.querySelector(".f-scores").innerHTML = jsonObjektas.rezultatas.komanda1;
+      document.querySelector(".s-scores").innerHTML = jsonObjektas.rezultatas.komanda2;
+      document.querySelector(".one").innerHTML = `<img class="logo one" src="${jsonObjektas.obj.logo.flogo}" alt="logo">`;
+      document.querySelector(".two").innerHTML = `<img class="logo two" src="${jsonObjektas.obj.logo.slogo}" alt="logo">`;
+
+      if (jsonObjektas.komanda1 === jsonObjektas.rezultatas.komanda2) {
+        document.querySelector(".f-scores").style.color = "#fff";
+        document.querySelector(".s-scores").style.color = "#fff";
+      } else if (jsonObjektas.komanda1 > jsonObjektas.rezultatas.komanda2) {
+        document.querySelector(".f-scores").style.color = "#00F2A7";
+      } else {
+        document.querySelector(".s-scores").style.color = "#00F2A7";
+      }
+
+      if (jsonObjektas.rezultatas.kelinys === 1) {
+        document.querySelector(".match-stage").innerHTML = `${jsonObjektas.rezultatas.kelinys}st quarter`;
+      } else if (jsonObjektas.rezultatas.kelinys === 2) {
+        document.querySelector(".match-stage").innerHTML = `${jsonObjektas.rezultatas.kelinys}nd quarter`;
+      } else if (jsonObjektas.rezultatas.kelinys === 3) {
+        document.querySelector(".match-stage").innerHTML = `${jsonObjektas.rezultatas.kelinys}rd quarter`;
+      } else if (jsonObjektas.rezultatas.kelinys === 4) {
+        document.querySelector(".match-stage").innerHTML = `${jsonObjektas.rezultatas.kelinys}th quarter`;
+      }
+      //console.log(jsonObjektas.quarter);
+    });
+
+  //console.log('test Zodis')
+  setTimeout(() => {
+    play();
+  }, 6000);
+};
+play();
+
+document.querySelector(".button").addEventListener("click", () => {
+  //console.log('clicked')  //spaudziam mygtuka narsyklej ir patikrinam konsolej, ar pajungtas eventListener
+  play();
+});
+
+
+
+
 document.querySelector(".renew").style.display = "none";
 document.querySelector(".one").style.display = "none";
 document.querySelector(".two").style.display = "none";
 document.querySelector(".match-stage").style.display = "none";
-
+document.querySelector(".scores").style.display = "none";
 
 document.querySelector(".start-match").addEventListener("click", () => {
   //console.log("test");
   //if (document.querySelector("#naujas-macas").style.display === "none") {
-    document.querySelector("#naujas-macas").style.display = "block";
- // } else {
- //   document.querySelector("#naujas-macas").style.display = "none";
+  document.querySelector("#naujas-macas").style.display = "block";
+  // } else {
+  //   document.querySelector("#naujas-macas").style.display = "none";
   //}
-  
+
   document.querySelector('#naujas-macas input[name="round"]').value = "";
   document.querySelector('#naujas-macas input[name="date"]').value = "";
   document.querySelector('#naujas-macas select[name="location"]').value = "";
@@ -20,14 +146,12 @@ document.querySelector(".start-match").addEventListener("click", () => {
   document.querySelector('#naujas-macas select[name="team-2"]').value = "";
   document.querySelector(".renew").innerHTML = "";
 
-  document.querySelector(".round").innerText = '';
-  document.querySelector(".date").innerHTML = '';
-  document.querySelector(".stadium").innerHTML = '';
-  document.querySelector(".time").innerHTML = '';
+  document.querySelector(".round").innerText = "";
+  document.querySelector(".date").innerHTML = "";
+  document.querySelector(".stadium").innerHTML = "";
+  document.querySelector(".time").innerHTML = "";
   document.querySelector(".bottom").style.display = "none";
 });
-
-
 
 document.querySelector("#run-match").addEventListener("click", (event) => {
   if (document.querySelector("#naujas-macas").style.display === "block") {
@@ -36,7 +160,7 @@ document.querySelector("#run-match").addEventListener("click", (event) => {
   event.preventDefault();
 
   let round = document.querySelector('#naujas-macas input[name="round"]').value;
-  document.querySelector('#naujas-macas input[name="round"]').value = "";
+  document.querySelector('#naujas-macas input[name="round"]').value = "";  //padarom tuscias reiksmes, kad paspaudus mygtuka "SEARCH" visos input reiksmes "nusinulintu'
   let date = document.querySelector('#naujas-macas input[name="date"]').value;
   document.querySelector('#naujas-macas input[name="date"]').value = "";
   let loc = document.querySelector('#naujas-macas select[name="location"]').value;
@@ -48,6 +172,13 @@ document.querySelector("#run-match").addEventListener("click", (event) => {
   let team2 = document.querySelector('#naujas-macas select[name="team-2"]').value;
   document.querySelector('#naujas-macas select[name="team-2"]').value = "";
 
+/*
+if(team1 === 'Olympiacos Piraeus'){
+  document.querySelector(".one").innerHTML = `<img class="logo one" src="${jsonObjektas.obj.logo.flogo}" alt="logo">`;
+} else {
+  document.querySelector(".two").innerHTML = `<img class="logo two" src="${jsonObjektas.obj.logo.slogo}" alt="logo">`;
+}
+*/
   fetch("http://localhost:3001/post-request", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -55,24 +186,21 @@ document.querySelector("#run-match").addEventListener("click", (event) => {
   })
     .then((resp) => resp.json())
     .then((resp) => {
-      console.log(resp);
+     console.log("resp", resp);
       document.querySelector(".round").innerText = `Round ${resp.round}`;
       document.querySelector(".date").innerHTML = resp.date;
       document.querySelector(".stadium").innerHTML = resp.loc;
       document.querySelector(".time").innerHTML = resp.time;
       document.querySelector(".first").innerHTML = resp.team1;
       document.querySelector(".second").innerHTML = resp.team2;
+
       document.querySelector(".bottom").style.display = "flex";
       document.querySelector(".renew").style.display = "none";
       document.querySelector(".one").style.display = "block";
       document.querySelector(".two").style.display = "block";
       document.querySelector(".match-stage").style.display = "block";
+      document.querySelector(".scores").style.display = "flex";
 
-    /*  if(team1 !== 'Olympiacos Piraeus') {
-        document.querySelector(".one").innerHTML = `<img src="../img/Istanbul.png" alt="logo">`;
-      }
-      if(team2 !== 'Anadolu Efes Istanbul') {
-        document.querySelector(".two").innerHTML = `<img src="../img/Olympiacos.png" alt="logo">`;
-      } */
     });
+
 });
